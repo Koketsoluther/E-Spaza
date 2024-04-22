b4e0af6b00b0c3d24835a3af1c2837f0195f6831
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-04-23 00:59:49 +0200

    revert changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React from 'react';
 import './index.css' ;
-import './components/LandingPage/Home.css'
-// import LoginButton from './components/LoginForm/LoginButton';
-// import LogoutButton from './components/LoginForm/LogoutButton';
-// import UserProfile from './components/UserProfile';
-import Home from './components/LandingPage/Home';
-// import About from "./Components/About";
-// import Work from "./Components/Work";
-// import Testimonial from "./Components/Testimonial";
-// import Contact from "./Components/Contact";
-// import Footer from "./Components/Footer";
+import LoginButton from './components/LoginForm/LoginButton';
+import LogoutButton from './components/LoginForm/LogoutButton';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
   
   return (
-    // <main className='column'>
-    //   <h1>Auth0 Login</h1>
-    //   <LoginButton/>
-    //   <LogoutButton/>
-    //   <UserProfile/>
-
-      <div className='App'>
-      <Home/>
-      {/* <About />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer /> */}
-
-      </div>
-    
-
-    
-
+     <main className='column'>
+       <h1>Auth0 Login</h1>
+       <LoginButton/>
+       <LogoutButton/>
+       <UserProfile/>
+      </main>
+      
   );
 }
 
--- src/components/LoginForm/LoginButton.js
+++ src/components/LoginForm/LoginButton.js
@@ -1,13 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from 'react';
-import { FiArrowRight } from "react-icons/fi";
 
 const LoginButton=()=>{
     const {loginWithRedirect,isAuthenticated}=useAuth0();
     return(
         !isAuthenticated &&(
             <button className="secondary-button" onClick={()=> loginWithRedirect()}>
-                Sign In <FiArrowRight />{" "} 
+                Sign In
             </button>
         )
      
