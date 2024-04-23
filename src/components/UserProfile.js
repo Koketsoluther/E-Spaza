e058eee5639cf0987f27b12e313b2dffdc81f75c
nothandonk <nothandonkambule1@gmail.com>
2024-04-23 13:02:03 +0200

    LOgin succesfull for shopper,admin and not in data

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,16 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from 'react';
-
+const users = [{email: "nothandonkambule1@gmail.com", Role: "Shopper"},{email: "iamluther@gmail.com", Role: "Admin"}];
+let userrole = "";
 const UserProfile=()=>{
-    const {user,isAuthenticated}=useAuth0();
-    return(
-        isAuthenticated &&(
-            <article className="column">
-                {user?.picture && <img src={user.picture} alt={user?.name}></img>}
-                <h2>{user?.name}</h2>
-                <p>Email: {user?.email}</p>
-                <p>Nickname: {user?.nickname}</p>
 
+    const {user,isAuthenticated}=useAuth0();
+    
+    let foundUser = false;
+    for (var userdt of users) {
+        if (userdt.email === user?.email) {
+            userrole = userdt.Role;
+            foundUser = true;
+            break;
+        }
+    }
+
+    return (
+        isAuthenticated && (
+            <article className="column">
+                {user?.picture && <img src={user.picture} alt={user?.name} />}
+                {foundUser ? (
+                    <h2>Welcome {userrole} {user?.name}</h2>
+                ) : (
+                    <h2>Sorry, you are not part of our database. Call helpline for assistance.</h2>
+                )}
             </article>
         )
      
