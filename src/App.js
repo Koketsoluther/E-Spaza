b2e1a07261d04691a1e5263da2f756fd0dc1a3bb
mellrose <2428123@students.wits.ac.za>
2024-04-28 12:26:49 +0200

    New branche

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import './index.css' ;
-import backgroundImage from "../src/components/Assets/E-Spaza.webp";
+import './index.css';
+import backgroundImage from '../src/components/Assets/E-Spaza.webp';
 import LoginButton from './components/LoginForm/LoginButton';
 import LogoutButton from './components/LoginForm/LogoutButton';
 import UserProfile from './components/UserProfile';
-
+import Products from './components/Products/products';
 
 function App() {
   const mainStyle = {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    color: 'white'
-    /* Add any other styles as needed */
+    color: 'blue'
+    // Add any other styles as needed
   };
+
   return (
-    <main className='column' style={mainStyle}>
-       <h1> E-spaza Login</h1>
-       <LoginButton/>
-       <LogoutButton/>
-       <UserProfile/>
+    <div>
+      <main className='column' style={mainStyle}>
+        <h1>E-spaza Login</h1>
+        <LoginButton />
+        <LogoutButton />
+        <UserProfile />
       </main>
-      
+      <Products />
+    </div>
   );
 }
 
