115e183a670febab304a7b5d9c3f6e726cf3c49f
nothandonk <nothandonkambule1@gmail.com>
2024-05-05 21:00:22 +0200

    listproduct added but not tested

--- admin/src/pages/ListProduct/listproduct.jsx
+++ admin/src/pages/ListProduct/listproduct.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './listproduct.css'
+import axios from "axios"
+import {toast} from "react-toastify"
 
-const Add = () => {
+const List = () => {
+    const url = ""
+    const [list,setList] = useState([]);
+
+    const fetchList =async () => {
+        const response =await axios.get(`${url}/api/product/list`);
+
+        if(response.data.success){
+            setList(response.data.data)
+        }
+        else{
+            toast.error("error")
+        }
+    }
+
+    const removeProduct = async(ProductID) => {
+        const response = await axios.post(`${url}/api/product/remove`,{is:ProductID});
+        await fetchList();
+        if (response.data.success){
+            toast.success(response.data.message)
+        }
+        else{
+            toast.error("Error")
+        }
+    }
+    useEffect(()=>{
+        fetchList();
+    },[])
     return (
-        <div>
-            list product
+        <div className='list add flex-col'>
+            <p>All product list</p>
+            <div className='list-table'>
+                <div className = "list-table-format title">
+                    <b>Image</b>
+                    <b>Name</b>
+                    <b>Category</b>
+                    <b>Price</b>
+                    <b>Action</b>
+                </div>
+                {list.map((item,index)=>{
+                    return (
+                       <div key={index} className='list-table-format'>
+                            <img src={`${url}/images/`+item.image} alt = "" />
+                            <p>{item.name}</p>
+                            <p>{item.category}</p>
+                            <p>${item.price}</p>
+                            <p onClick={()=>removeProduct(item._id)} className='cursor'>X</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
 
-export default Add
+export default List
