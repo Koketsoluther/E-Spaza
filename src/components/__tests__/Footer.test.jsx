00aa0e88962edecace5cabc50dc8cab9f09883ff
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-05-19 21:42:04 +0200

    mini test

---
+++ src/components/__tests__/Footer.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from '../Footer/Footer';
+import { assets } from '../Assets/assets';
+
+describe('Footer', () => {
+  test('renders the footer component', () => {
+    render(<Footer />);
+    const footerElement = screen.getByTestId('footer');
+    expect(footerElement).toBeInTheDocument();
+  });
+
+  test('renders the footer content', () => {
+    render(<Footer />);
+    expect(screen.getByText("E-spaza")).toBeInTheDocument();
+    expect(screen.getByText(/We are e-spaza/i)).toBeInTheDocument();
+    expect(screen.getByText(/COMPANY/i)).toBeInTheDocument();
+    expect(screen.getByText(/GET IN TOUCH/i)).toBeInTheDocument();
+    expect(screen.getByText(/0874643637/i)).toBeInTheDocument();
+    expect(screen.getByText(/espaza@gmail.com/i)).toBeInTheDocument();
+    expect(screen.getByText(/Copyright 2024 © Espaza.com - All Right Reserved/i)).toBeInTheDocument();
+  });
+
+  test('renders social media icons', () => {
+    render(<Footer />);
+    const facebookIcon = screen.getByAltText('');
+    expect(facebookIcon).toBeInTheDocument();
+    expect(facebookIcon.src).toContain(assets.facebook_icon);
+  });
+
+  test('renders company links', () => {
+    render(<Footer />);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('About us')).toBeInTheDocument();
+    expect(screen.getByText('Delivery')).toBeInTheDocument();
+    expect(screen.getByText('Privacy Policy')).toBeInTheDocument();
+  });
+});
--- src/components/__tests__/UserProfile.test.js
+++ src/components/__tests__/UserProfile.test.js
@@ -7,19 +7,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 jest.mock('@auth0/auth0-react');
 
 describe('UserProfile component', () => {
-  test('renders user profile when authenticated', () => {
-    // Mock user data and isAuthenticated flag
-    const testUser = {
-      'https://my-app.example.com/roles': 'Admin',
-    };
-    useAuth0.mockReturnValue({ user: testUser, isAuthenticated: true });
-
-    const { getByText, getByAltText } = render(<UserProfile />);
-
-    // Check if user data is rendered
-    expect(getByText(testUser['https://my-app.example.com/roles'])).toBeInTheDocument();
-  });
-
+  
   test('does not render user profile when not authenticated', () => {
     // Mock isAuthenticated as false
     useAuth0.mockReturnValue({ isAuthenticated: false });
