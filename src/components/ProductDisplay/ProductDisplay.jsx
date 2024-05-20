e0eea51231f2c557c311de34a7cfe6a50c75f4e3
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-05-20 17:43:44 +0200

    pi

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -7,7 +7,7 @@ const FoodDisplay=({category})=>{
     const {foodData}= useContext(StoreContext)
     return(
         <div className='product-display' id='product-display'>
-            <h2>Top products near you</h2>
+            <h2>Top Products near you</h2>
             <div className='product-display-list'>
                 {foodData.map((item,index)=>{
                     console.log(category,item.CATEGORY);
